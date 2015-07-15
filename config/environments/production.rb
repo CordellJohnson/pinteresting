4c6173810936a43a7340b2f71593204e9c826b6a
Cordell Johnson <cordell.johnson188@gmail.com>
2015-07-14 21:09:55 -0400

    implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Required fo Heroku
+  # Not to set this to your actual host name
+
+  config.action_mailer.default_url_options = { host: => "zummly.com" }
+
 end
